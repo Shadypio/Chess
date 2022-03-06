56f37a126ea4621de174d28b2e385243d7735ac8
Vincenzopio Amendola <enzuccioamendola@gmail.com>
2022-03-06 20:31:26 +0100

    Promozione, arrocco, en passant

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -4,6 +4,7 @@ Inoltre, verifica quali sono le mosse valide nello stato corrente e tiene tracci
 delle mosse compiute.
 """
 
+import pygame as p
 
 class GameState():
     def __init__(self):
@@ -29,11 +30,15 @@ class GameState():
         self.whiteKingLocation = (7, 4)
         self.blackKingLocation = (0, 4)
         self.checkMate = False
-        self.staleMatae = False
+        self.staleMate = False
+        self.enPassantPossible = ()  # Coordinate della cella disponibile per en passant
+        self.currentCastlingRight = CastleRights(True, True, True, True)
+        self.castleRightsLog = [CastleRights(self.currentCastlingRight.wks, self.currentCastlingRight.bks,
+                                             self.currentCastlingRight.wqs, self.currentCastlingRight.bqs)]
+
 
     '''
-    Prende una mossa e la esegue 
-    (Arrocco, promozione ed en-passant non funzionanti)
+    Esamina una mossa e la esegue 
     '''
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
@@ -45,6 +50,33 @@ class GameState():
             self.whiteKingLocation = (move.endRow, move.endCol)
         if move.pieceMoved == 'bK':
             self.blackKingLocation = (move.endRow, move.endCol)
+
+        # Promozione pedone a Regina
+        if move.isPawnPromotion:
+            self.board[move.endRow][move.endCol] = move.pieceMoved[0] + 'Q'
+
+        # EnPassant
+        if move.isEnPassantMove:
+            self.board[move.startRow][move.endCol] = '--'
+
+        if move.pieceMoved[1] == 'p' and abs(move.startRow - move.endRow) == 2:  # Solo se il pedone avanza di due case
+            self.enPassantPossible = ((move.startRow + move.endRow)//2, move.startCol)
+        else:
+            self.enPassantPossible = ()
+
+        # Arrocco
+        if move.isCastleMove:
+            if move.endCol - move.startCol == 2:  # Arrocco corto
+                self.board[move.endRow][move.endCol-1] = self.board[move.endRow][move.endCol+1]  # Muove la torre nella nuova casella
+                self.board[move.endRow][move.endCol+1] = '--'  # Toglie la torre dalla vecchia posizione
+            else:  # Arrocco lungo
+                self.board[move.endRow][move.endCol+1] = self.board[move.endRow][move.endCol-2]  # Muove la torre nella nuova casella
+                self.board[move.endRow][move.endCol-2] = '--'
+
+        # Aggiorna diritto ad arrocco - se re o torre vengono mossi
+        self.updateCastleRights(move)
+        self.castleRightsLog.append(CastleRights(self.currentCastlingRight.wks, self.currentCastlingRight.bks,
+                                             self.currentCastlingRight.wqs, self.currentCastlingRight.bqs))
 
     '''
     Annulla la mossa precedente
@@ -60,13 +92,68 @@ class GameState():
                 self.whiteKingLocation = (move.startRow, move.startCol)
             if move.pieceMoved == 'bK':
                 self.blackKingLocation = (move.startRow, move.startCol)
+            # Annulla en passant
+            if move.isEnPassantMove:
+                self.board[move.endRow][move.endCol] = '--'
+                self.board[move.startRow][move.endCol] = move.pieceCaptured
+                self.enPassantPossible = (move.endRow, move.endCol)
+            # Annulla un avanzamento di 2 case di un pedone
+            if move.pieceMoved[1] == 'p' and abs(move.startRow - move.endRow) == 2:
+                self.enPassantPossible = ()
+
+            # Annulla diritto ad arrocco
+            self.castleRightsLog.pop()
+            newRights = self.castleRightsLog[-1]
+            self.currentCastlingRight = CastleRights(newRights.wks, newRights.bks, newRights.wqs, newRights.bqs)
+
+            # Annulla arrocco
+            if move.isCastleMove:
+                if move.endCol - move.startCol == 2:  # Arrocco corto
+                    self.board[move.endRow][move.endCol+1] = self.board[move.endRow][move.endCol-1]
+                    self.board[move.endRow][move.endCol-1] = "--"
+                else:  # Arrocco lungo
+                    self.board[move.endRow][move.endCol-2] = self.board[move.endRow][move.endCol+1]
+                    self.board[move.endRow][move.endCol+1] = '--'
+
+
+
+    '''
+    Aggiorna il diritto ad arrocco della mossa move
+    '''
+    def updateCastleRights(self, move):
+        if move.pieceMoved == 'wK':
+            self.currentCastlingRight.wks = False
+            self.currentCastlingRight.wqs = False
+        elif move.pieceMoved == 'bK':
+            self.currentCastlingRight.bks = False
+            self.currentCastlingRight.bqs = False
+        elif move.pieceMoved == 'wR':
+            if move.startRow == 7:
+                if move.startCol == 0:  # Torre sinistra
+                    self.currentCastlingRight.wqs = False
+                elif move.startCol == 7:  # Torre destra
+                    self.currentCastlingRight.wks = False
+        elif move.pieceMoved == 'bR':
+            if move.startRow == 0:
+                if move.startCol == 0:  # Torre sinistra
+                    self.currentCastlingRight.bqs = False
+                elif move.startCol == 7:  # Torre destra
+                    self.currentCastlingRight.bks = False
+
 
     '''
     Mosse con scacco (con inchiodatura)
     '''
     def getValidMoves(self):
+        tempEnPassantPossible = self.enPassantPossible
+        tempCastleRights = CastleRights(self.currentCastlingRight.wks, self.currentCastlingRight.bks,
+                                        self.currentCastlingRight.wqs, self.currentCastlingRight.bqs)
         # 1.) Generare tutte le mosse
         moves = self.getAllPossibleMoves()
+        if self.whiteToMove:
+            self.getCastleMoves(self.whiteKingLocation[0], self.whiteKingLocation[1], moves)
+        else:
+            self.getCastleMoves(self.blackKingLocation[0], self.blackKingLocation[1], moves)
 
         # 2.) Per ogni mossa, fai la mossa
         for i in range(len(moves)-1, -1, -1):  # Quando rimuovi dalla lista, naviga all'indietro
@@ -92,6 +179,8 @@ class GameState():
             self.checkMate = False
             self.staleMate = False
 
+        self.enPassantPossible = tempEnPassantPossible
+        self.currentCastlingRight = tempCastleRights
         return moves
 
     '''
@@ -102,6 +191,7 @@ class GameState():
             return self.squareUnderAttack(self.whiteKingLocation[0], self.whiteKingLocation[1])
         else:
             return self.squareUnderAttack(self.blackKingLocation[0], self.blackKingLocation[1])
+
     '''
     Verifica se il nemico attacca la casa r, c
     '''
@@ -140,9 +230,13 @@ class GameState():
             if c-1 > 0:  # Cattura a sinistra
                 if self.board[r-1][c-1][0] == 'b':  # Pezzo nemico da catturare
                     moves.append(Move((r, c), (r-1, c-1), self.board))
+                elif (r-1, c-1) == self.enPassantPossible:
+                    moves.append(Move((r, c), (r - 1, c - 1), self.board, isEnPassantMove=True))
             if c+1 < 7:  # Cattura a destra
                 if self.board[r-1][c+1][0] == 'b':  # Pezzo nemico da catturare
                     moves.append(Move((r, c), (r-1, c+1), self.board))
+                elif (r - 1, c + 1) == self.enPassantPossible:
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board, isEnPassantMove=True))
 
         else:  # Pedone nero
             if self.board[r + 1][c] == "--":  # Se la cella avanti è vuota
@@ -152,9 +246,13 @@ class GameState():
             if c - 1 >= 0:  # Cattura a sinistra
                 if self.board[r + 1][c - 1][0] == 'w':  # Pezzo nemico da catturare
                     moves.append(Move((r, c), (r + 1, c - 1), self.board))
+                elif (r + 1, c - 1) == self.enPassantPossible:
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board, isEnPassantMove=True))
             if c + 1 <= 7:  # Cattura a destra
                 if self.board[r + 1][c + 1][0] == 'w':  # Pezzo nemico da catturare
                     moves.append(Move((r, c), (r + 1, c + 1), self.board))
+                elif (r + 1, c + 1) == self.enPassantPossible:
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board, isEnPassantMove=True))
         # Aggiungere promozione pedone
 
     '''
@@ -236,6 +334,33 @@ class GameState():
                 if endPiece[0] != allyColor:  # Non è un pezzo amico (vuoto o pezzo nemico)
                     moves.append(Move((r, c), (endRow, endCol), self.board))
 
+    '''
+    Genera le mosse per arroccare
+    '''
+    def getCastleMoves(self, r, c, moves):
+        if self.squareUnderAttack(r, c):
+            return  # Impossibile arroccare sotto scacco
+        if (self.whiteToMove and self.currentCastlingRight.wks) or (not self.whiteToMove and self.currentCastlingRight.bks):
+            self.getKingSideCastleMoves(r, c, moves)
+        if (self.whiteToMove and self.currentCastlingRight.wqs) or (not self.whiteToMove and self.currentCastlingRight.bqs):
+            self.getQueenSideCastleMoves(r, c, moves)
+
+    def getKingSideCastleMoves(self, r, c, moves):
+        if self.board[r][c+1] == '--' and self.board[r][c+2] == '--':
+            if not self.squareUnderAttack(r, c+1) and not self.squareUnderAttack(r, c+2):
+                moves.append(Move((r, c), (r, c+2), self.board, isCastleMove=True))
+
+    def getQueenSideCastleMoves(self, r, c, moves):
+        if self.board[r][c-1] == '--' and self.board[r][c-2] == '--' and self.board[r][c-3 == '--']:
+            if not self.squareUnderAttack(r, c-1) and not self.squareUnderAttack(r, c-2):
+                moves.append(Move((r, c), (r, c-2), self.board, isCastleMove=True))
+
+class CastleRights():
+    def __init__(self, wks, bks, wqs, bqs):
+        self.wks = wks
+        self.bks = bks
+        self.wqs = wqs
+        self.bqs = bqs
 
 class Move():
     # Mappa le chiavi ai valori
@@ -247,13 +372,25 @@ class Move():
                    "e": 4, "f": 5, "g": 6, "h": 7}
     colsToFiles = {v: k for k, v in filesToCols.items()}
 
-    def __init__(self, startSq, endSq, board):
+    def __init__(self, startSq, endSq, board, isEnPassantMove=False, isCastleMove=False):
         self.startRow = startSq[0]
         self.startCol = startSq[1]
         self.endRow = endSq[0]
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+
+        # Promozione
+        self.isPawnPromotion = (self.pieceMoved == 'wp' and self.endRow == 0) or (self.pieceMoved == 'bp' and self.endRow == 7)
+
+        # En passant
+        self.isEnPassantMove = isEnPassantMove
+        if self.isEnPassantMove:
+            self.pieceCaptured = 'wp' if self.pieceMoved == 'bp' else 'bp'
+
+        # Arrocco
+        self.isCastleMove = isCastleMove
+
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
 
     '''
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -57,12 +57,13 @@ def main():
                 if len(playerClicks) == 2:  # Dopo il secondo clic
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    if move in validMoves:
-                        gs.makeMove(move)
-                        moveMade = True
-                        sqSelected = ()  # Resetta i clic dell'utente
-                        playerClicks = []
-                    else:
+                    for i in range(len(validMoves)):
+                        if move == validMoves[i]:
+                            gs.makeMove(validMoves[i])
+                            moveMade = True
+                            sqSelected = ()  # Resetta i clic dell'utente
+                            playerClicks = []
+                    if not moveMade:
                         playerClicks = [sqSelected]
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:  # Annulla la mossa con Z
