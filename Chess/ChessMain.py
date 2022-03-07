d43110345f872a089aa19d9ee492bddb4b3330c3
Vincenzopio Amendola <enzuccioamendola@gmail.com>
2022-03-07 10:01:19 +0100

    Mosse avversarie generate casualmente (algoritmo base)

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -4,7 +4,7 @@ corrente.
 """
 
 import pygame as p
-from Chess import ChessEngine
+from Chess import ChessEngine, SmartMoveFinder
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -41,13 +41,18 @@ def main():
     sqSelected = ()  # Tiene traccia del clic dell'utente
     playerClicks = []  # Tiene traccia dei clic dell'utente (due tuple: pezzo selezionato e destinazione)
     gameOver = False
+    # Utente e Computer
+    playerOne = True  # Se l'utente gioca il bianco la variabile è True, altrimenti False
+    playerTwo = False  # Uguale a playerOne
 
     while running:
+        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
+
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not gameOver:
+                if not gameOver and humanTurn:
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
@@ -81,6 +86,13 @@ def main():
                     playerClicks = []
                     moveMade = False
                     animate = False
+
+        # Ricerca mosse AI
+        if not gameOver and not humanTurn:
+            AIMove = SmartMoveFinder.findRandomMove(validMoves)
+            gs.makeMove(AIMove)
+            moveMade = True
+            animate = True
 
         if moveMade:
             if animate:
